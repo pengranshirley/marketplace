9365d0ebf353f312ee3b60e195d8e656adb8160b
pengranshirley <zhaopengran@gmail.com>
2013-07-02 15:03:40 +0800

    py files updated

--- giftbook.py
+++ giftbook.py
@@ -12,7 +12,6 @@ jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
 
-
 # Datastore definitions
 class Persons(db.Model):
   """Models a person identified by email"""
@@ -60,110 +59,120 @@ class MainPage(webapp2.RequestHandler):
 
 class registration(webapp2.RequestHandler):
   def get(self):
-      template = jinja_environment.get_template('signup.html')
-      self.response.out.write(template.render())
+      user = users.get_current_user()
+      if user:
+        template = jinja_environment.get_template('signup.html')
+        self.response.out.write(template.render())
+      else:
+        self.redirect("/marketplace")
 
 class SellForm(webapp2.RequestHandler):
   def get(self):
-      template = jinja_environment.get_template('sell.html')
-      self.response.out.write(template.render())
+      user = users.get_current_user()
+      if user:
+        template = jinja_environment.get_template('sell.html')
+        self.response.out.write(template.render())
+      else:
+        self.redirect("/marketplace")
+
 
 
 # Handle user registration, corresponds to "register"
 class AddUser(webapp2.RequestHandler):
   def post(self):
-    email = users.get_current_user().email()
-    newPerson = Persons(key_name=email)
-    newPerson.email = email
-    newPerson.description = self.request.get('user_description')
-    newPerson.username = self.request.get('username')
-    newPerson.put()
-    #afterwards, redirect to the main page (9 categories)
-    template_values = {
-          'user_mail': users.get_current_user().email(),
-          'logout': users.create_logout_url(self.request.host_url),
-          'username': newPerson.username,
-    } 
-    template = jinja_environment.get_template('frontuser.html')
-    self.response.out.write(template.render(template_values))
+    user = users.get_current_user()
+    if user:
+         email = users.get_current_user().email()
+         newPerson = Persons(key_name=email)
+         newPerson.email = email
+         newPerson.description = self.request.get('user_description')
+         newPerson.username = self.request.get('username')
+         newPerson.put()
+         #afterwards, redirect to the main page (9 categories)
+         template_values = {
+            'user_mail': users.get_current_user().email(),
+            'logout': users.create_logout_url(self.request.host_url),
+            'username': newPerson.username,
+         } 
+         template = jinja_environment.get_template('frontuser.html')
+         self.response.out.write(template.render(template_values))
+    else:
+        self.redirect("/marketplace")
 
 
 class AddItem(webapp2.RequestHandler):
   def post(self):
-    parent_key = db.Key.from_path('Persons', users.get_current_user().email())
-    person = db.get(parent_key)
-    item = Items(parent=parent_key)
-    item.description = self.request.get('desc')
-    item.category = self.request.get("item_category")
-    item.condition = self.request.get("item_condition")
-    item.name = self.request.get("item_name")
-    item.location = self.request.get("item_location")
-    item.image_link = self.request.get('image_url')
-    item.put()
+    user = users.get_current_user()
+    if user:
+        parent_key = db.Key.from_path('Persons', users.get_current_user().email())
+        person = db.get(parent_key)
+        item = Items(parent=parent_key)
+        item.description = self.request.get('desc')
+        item.category = self.request.get("item_category")
+        item.condition = self.request.get("item_condition")
+        item.name = self.request.get("item_name")
+        item.location = self.request.get("item_location")
+        item.image_link = self.request.get('image_url')
+        item.put()
 
-    template_values = {
-          'user_mail': users.get_current_user().email(),
-          'logout': users.create_logout_url(self.request.host_url),
-          'item': item,
-          'owner': person,
-    } 
-    template = jinja_environment.get_template('product.html')
-    self.response.out.write(template.render(template_values))
+        template_values = {
+              'user_mail': users.get_current_user().email(),
+              'logout': users.create_logout_url(self.request.host_url),
+              'item': item,
+              'owner': person,
+        } 
+        template = jinja_environment.get_template('product.html')
+        self.response.out.write(template.render(template_values))
+    else:
+        self.redirect("/marketplace")
 
-
-    #img = db.BlobProperty() hasn't implemented yet
-    #divert to product display page
-    """
-    self.redirect('/viewProduct?ID='+item.Key().id()+"user="+parent_key)
-    """
   
 
 class DisplayByC(webapp2.RequestHandler):
   def get(self, name):
+        category = self.request.get("c")
+        page = self.request.get("p") 
+        start_no = int((int(page)-1)*9)
+        query = db.GqlQuery("SELECT * "
+                            "FROM Items "
+                            "WHERE category = :1 "
+                            "ORDER BY date DESC",
+                            category)
     
-    category = self.request.get("c")
-    page = self.request.get("p")
-    
-    start_no = (int(page)-1)*9
-    query = db.GqlQuery("SELECT * " +
-                        "FROM Items " +
-                        "ORDER BY date DESC" +
-                        "LIMIT 9" +
-                        "OFFSET :1" +
-                        "WHERE category = :2", start_no, category)
-    
+        query_2 = query.fetch(9, int(start_no))
 
-
-    template_values = {
-      'user_mail': users.get_current_user().email(),
-      'logout': users.create_logout_url(self.request.host_url),
-      'category': category,
-      'page': page,
-      'items': query,
-    }
-    template = jinja_environment.get_template('category.html')
-    self.response.out.write(template.render(template_values)) 
+        template_values = {
+          'user_mail': users.get_current_user().email(),
+          'logout': users.create_logout_url(self.request.host_url),
+          'category': category,
+          'page': page,
+          'items': query_2,
+        }
+        template = jinja_environment.get_template('category.html')
+        self.response.out.write(template.render(template_values)) 
     
-    #Assume page no is always valid
-    #Suppose one page display 9 products
-    #Return all 9 items
+        #Assume page no is always valid
+        #Suppose one page display 9 products
+        #Return all 9 items
 
 class DisplayProduct(webapp2.RequestHandler):
-  def get(self):
+  def get(self, name):
     key = self.request.get("ID")  #id is each product's natural primary key
-    parent = self.request.get("user")
-    person = db.get(parent_key)
-    item = Items.get_by_id(key, parent = person)
+    parent_email = self.request.get("user")
+    person = db.Key.from_path('Persons', parent_email)
+
+    item = Items.get_by_id(int(key), parent = person)
     template_values = {
       'user_mail': users.get_current_user().email(),
       'logout': users.create_logout_url(self.request.host_url),
       'item': item,
       'owner':person,
     }
+
     template = jinja_environment.get_template('product.html')
     self.response.out.write(template.render(template_values))
 
-app = webapp2.WSGIApplication([('/login', MainPage),
+app = webapp2.WSGIApplication([('/marketplace', MainPage),
                                ('/signup',registration),
                                ('/register', AddUser),
                                ('/sell', SellForm),
